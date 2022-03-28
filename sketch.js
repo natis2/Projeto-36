57a7f846e55912df0a4527f6c8854ad14983e093
natis2 <94996586+natis2@users.noreply.github.com>
2022-03-28 15:21:03 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ var question, contestant, quiz;
 
 function setup(){
   canvas = createCanvas(850,400);
-  database = firebase.database();
+ // database = firebase.database();
   quiz = new Quiz();
   quiz.start();
 }
